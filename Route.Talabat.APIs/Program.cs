03e328a53041185dfa1ddf07444e7a6005a89b72
amrtlt95 <amrtlt90@gmail.com>
2024-04-10 20:22:54 +0200

    Update Database

--- Route.Talabat.APIs/Program.cs
+++ Route.Talabat.APIs/Program.cs
@@ -6,12 +6,13 @@ namespace Route.Talabat.APIs
 {
 	public class Program
 	{
-		public static void Main(string[] args)
+		public static async Task Main(string[] args)
 		{
 			var webApplicationBuilder = WebApplication.CreateBuilder(args);
 
 			// Add services to the container.
 
+			#region Adding Services
 			webApplicationBuilder.Services.AddControllers();
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			webApplicationBuilder.Services.AddEndpointsApiExplorer();
@@ -19,11 +20,31 @@ namespace Route.Talabat.APIs
 			webApplicationBuilder.Services.AddDbContext<ApplicationDbContext>(options =>
 			{
 				options.UseSqlServer(webApplicationBuilder.Configuration.GetConnectionString("DefaultConnection"));
-			});
+			}); 
+			#endregion
 
 			var app = webApplicationBuilder.Build();
 
+			#region Applying Any Pending Migrations
+			using var scope = app.Services.CreateScope();
+			var services = scope.ServiceProvider;
+			var _dbContext = services.GetRequiredService<ApplicationDbContext>();
+			var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+			try
+			{
+				await _dbContext.Database.MigrateAsync();
+
+			}
+			catch (Exception ex)
+			{
+				var logger = loggerFactory.CreateLogger<Program>();
+				logger.LogError(ex, "an error occured during applying the Migrations");
+			}			
+			#endregion
+
 			// Configure the HTTP request pipeline.
+			#region Configure Kestrel Middlewares
 			if (app.Environment.IsDevelopment())
 			{
 				app.UseSwagger();
@@ -35,7 +56,8 @@ namespace Route.Talabat.APIs
 			app.UseAuthorization();
 
 
-			app.MapControllers();
+			app.MapControllers(); 
+			#endregion
 
 			app.Run();
 		}
