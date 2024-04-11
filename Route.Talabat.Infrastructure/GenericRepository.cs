298b0a804b8f1dfc000cded49572ba4953d64612
amrtlt95 <amrtlt90@gmail.com>
2024-04-11 06:39:01 +0200

    GenericRepository

---
+++ Route.Talabat.Infrastructure/GenericRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Route.Talabat.Core.Entities;
+using Route.Talabat.Core.Repositories.Contract;
+using Route.Talabat.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.Talabat.Infrastructure
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public GenericRepository(ApplicationDbContext DbContext)
+        {
+            _dbContext = DbContext;
+        }
+        public async Task<IEnumerable<T>> GetAllAsync() => await _dbContext.Set<T>().AsNoTracking().ToListAsync();
+
+
+        public async Task<T?> GetAsync(int id) => await _dbContext.Set<T>().FindAsync(id);
+    }
+}
