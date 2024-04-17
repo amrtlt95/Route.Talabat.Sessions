aef4b668d5ead2f24aa1e8b8ba3e7cdde8203c46
amrtlt95 <amrtlt90@gmail.com>
2024-04-17 12:22:39 +0200

    NotFound, BadRequest and Unauthorized Error Handling

---
+++ Route.Talabat.APIs/Controllers/BuggyController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Route.Talabat.Infrastructure.Data;
+
+namespace Route.Talabat.APIs.Controllers
+{
+
+    public class BuggyController : _BaseController
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public BuggyController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundError()
+        {
+            return NotFound();
+        }
+
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+            string s = null;
+            int x =s.Length;
+            return Ok();
+        }
+
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequestError()
+        {
+            return BadRequest();
+        }
+
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetBadRequestError(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("unauthorized")]
+        public ActionResult GetUnauthorizedError()
+        {
+            return Unauthorized();
+        }
+
+
+    }
+}
---
+++ Route.Talabat.APIs/Errors/ApiResponse.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Route.Talabat.APIs.Errors
+{
+    public class ApiResponse
+    {
+        public int StatusCode { get; set; }
+        public string? Message { get; set; } = null!;
+
+        public ApiResponse(int statusCode , string? message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetMessage(statusCode);
+        }
+
+        private string? GetMessage(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "Bad Request",
+                401 => "Unauthorized",
+                404 => "Not Found",
+                500 => "Server Error",
+                _ => null
+            };
+        }
+    }
+}
