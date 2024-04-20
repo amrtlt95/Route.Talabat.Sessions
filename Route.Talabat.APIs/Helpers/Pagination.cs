1d9c27b1d3dec377e9887c77967cd77d7d1a5fbc
amrtlt95 <amrtlt90@gmail.com>
2024-04-20 08:42:33 +0200

    Pagination

---
+++ Route.Talabat.APIs/Helpers/Pagination.cs
@@ -0,0 +1,18 @@
+﻿namespace Route.Talabat.APIs.Helpers
+{
+    public class Pagination<T>
+    {
+        public int Count { get; set; }
+        public int PageSize { get; set; }
+        public int PageIndex { get; set; }
+        public IReadOnlyList<T> Data { get; set; } = null!;
+
+        public Pagination(int pageSize,int pageIndex,int count, IReadOnlyList<T> data) 
+        {
+            PageSize = pageSize;
+            PageIndex = pageIndex;
+            Data = data;
+            Count = count;
+        }
+    }
+}
