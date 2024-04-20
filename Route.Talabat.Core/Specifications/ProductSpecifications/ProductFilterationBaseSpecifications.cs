88118894d0c97cb9a393115627d8b88f718e3af1
amrtlt95 <amrtlt90@gmail.com>
2024-04-20 08:41:40 +0200

    Refactor ProductSpecifiacation to Create CountSpecs

---
+++ Route.Talabat.Core/Specifications/ProductSpecifications/ProductFilterationBaseSpecifications.cs
@@ -0,0 +1,23 @@
+﻿using Route.Talabat.Core.Entities.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.Talabat.Core.Specifications.ProductSpecifications
+{
+    public abstract class ProductFilterationBaseSpecifications : BaseSpecifications<Product>
+    {
+        public ProductFilterationBaseSpecifications(ProductSpecParams productSpecParams) : base
+            (
+            p =>
+                (!productSpecParams.BrandId.HasValue || p.BrandId == productSpecParams.BrandId) && (!productSpecParams.CategoryId.HasValue || p.CategoryId == productSpecParams.CategoryId)
+            )
+        { }
+
+        public ProductFilterationBaseSpecifications(Expression<Func<Product,bool>> expression) : base(expression)
+        {}
+    }
+}
---
+++ Route.Talabat.Core/Specifications/ProductSpecifications/ProductWithBrandAndCategorySpecifications.cs
@@ -0,0 +1,49 @@
+﻿using Route.Talabat.Core.Entities.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.Talabat.Core.Specifications.ProductSpecifications
+{
+    public class ProductWithBrandAndCategorySpecifications : ProductFilterationBaseSpecifications
+    {
+        public ProductWithBrandAndCategorySpecifications(ProductSpecParams productSpecParams) : base(productSpecParams)
+        {
+            if (!string.IsNullOrEmpty(productSpecParams.Sort))
+            {
+                switch (productSpecParams.Sort)
+                {
+                    case "PriceAsc":
+                        OrderBy = p => p.Price;
+                        break;
+                    case "PriceDesc":
+                        OrderByDesc = p => p.Price;
+                        break;
+                    default:
+                        OrderBy = p => p.Name;
+                        break;
+
+                }
+            }
+            AddingIncludes();
+
+            AddPagination((productSpecParams.PageIndex - 1) * productSpecParams.PageSize, productSpecParams.PageSize);
+        }
+
+        public ProductWithBrandAndCategorySpecifications(int id) : base(p => p.Id == id)
+        {
+            AddingIncludes();
+        }
+
+        #region Helper methods
+        private void AddingIncludes()
+        {
+            Includes.Add(p => p.Brand);
+            Includes.Add(p => p.Category);
+        }
+        #endregion
+
+    }
+}
