06303ca0b4b15040bb004192f042642350664478
amrtlt95 <amrtlt90@gmail.com>
2024-04-23 08:49:36 +0200

    01-Basket Module

---
+++ Route.Talabat.Core/Entities/Basket/CustomerBasket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.Talabat.Core.Entities.Basket
+{
+    public class CustomerBasket
+    {
+        public string Id { get; set; } = null!;
+        public List<BasketItem> Items { get; set; } = null!;
+
+        public CustomerBasket(string id) {
+            Id = id;
+            Items = [];
+        }
+    }
+}
